1bfd382180db3b7bb1fca6942415484de2047ed9
Nur Hidayat <97712182+nurhdyt354@users.noreply.github.com>
2023-12-22 10:39:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const app = express()
 const mongoose = require('mongoose')
 
 
-mongoose.connect(
-  "mongodb+srv://dbFish:dbFishPassword@atlascluster.jmvsnrd.mongodb.net/", 
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-).then(() => {
-  console.log('Connected to DB');
-}).catch((err) => {
-  console.error('Connection error:', err);
-});
-                      
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    // Handle the GET request for the root path
+    res.send('Hello, World!');
+  });
 const FishRouter = require('./routes/Fish')
 app.use('/Fish', FishRouter)
+
 
 app.listen(3000, () => console.log('Server Started'))
